ceec748845a2d436ef65d341c88e30489133ee7f
DongWoo Lee <127951186+dlehddn@users.noreply.github.com>
2023-09-18 04:08:05 +0900

    feat : webhook test

--- back-was/src/main/java/was/backwas/BackWasApplication.java
+++ back-was/src/main/java/was/backwas/BackWasApplication.java
@@ -2,7 +2,7 @@ package was.backwas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 public class BackWasApplication {
 
