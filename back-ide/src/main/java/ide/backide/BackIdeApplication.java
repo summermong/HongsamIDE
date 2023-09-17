7c8f80dba1ac7af9d992aca1d87fcb5e543279e6
DongWoo Lee <127951186+dlehddn@users.noreply.github.com>
2023-09-18 05:59:28 +0900

    Update BackIdeApplication.java

--- back-ide/src/main/java/ide/backide/BackIdeApplication.java
+++ back-ide/src/main/java/ide/backide/BackIdeApplication.java
@@ -2,7 +2,7 @@ package ide.backide;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 public class BackIdeApplication {
 
