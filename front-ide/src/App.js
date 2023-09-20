e7f8eae5a7a76bfe4ab774a70e9fa8eafe11c257
강경규 <qwzx16@naver.com>
2023-09-20 18:39:23 +0900

    feat:test

--- front-ide/src/App.js
+++ front-ide/src/App.js
@@ -1,19 +1,22 @@
+/** @format */
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <div>브런치 잘 옮겨졌는가?</div>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
