058e2d461aea34f02dad9f15f497fd9cb87e0da1
summermong <2514o_o@naver.com>
2023-09-21 21:40:45 +0900

    feat: 홈 화면 추가

--- front-main/src/Pages/Home.js
+++ front-main/src/Pages/Home.js
@@ -1,16 +1,16 @@
-import Container from "../components/Container";
-import styles from "./Home.module.css";
+import Container from '../components/Container';
+import styles from './Home.module.css';
 
 const Home = () => {
   const containers = [
     [
-      { containerId: "1", title: "회의실 배정" },
-      { containerId: "2", title: "거북이" },
-      { containerId: "3", title: "회의실 배정" },
-      { containerId: "4", title: "회의실 배정" },
-      { containerId: "5", title: "회의실 배정" },
-      { containerId: "6", title: "엉금엉금" },
-      { containerId: "7", title: "앙금앙금" },
+      { containerId: '1', title: '토끼' },
+      { containerId: '2', title: '거북이' },
+      { containerId: '3', title: '회의실 배정' },
+      { containerId: '4', title: '회의실 배정' },
+      { containerId: '5', title: '회의실 배정' },
+      { containerId: '6', title: '엉금엉금' },
+      { containerId: '7', title: '앙금앙금' },
     ],
   ];
 
@@ -19,7 +19,7 @@ const Home = () => {
       <div className={styles.Content}>
         <div className={styles.title}>문제 목록</div>
         {containers.map((container) => (
-          <Container key={container.containerId} data={container} />
+          <Container key={container[0].containerId} data={container} />
         ))}
       </div>
     </div>
