c77ece3c6432efefbcb3fa06bd2402f962a7df87
summermong <2514o_o@naver.com>
2023-09-22 11:33:43 +0900

    feat: 회원가입 Ajax 코드

--- front-main/src/Pages/Signup.js
+++ front-main/src/Pages/Signup.js
@@ -1,5 +1,6 @@
 import React, { useForm } from 'react-hook-form';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './Signup.module.css';
 import axios from 'axios';
 
@@ -11,28 +12,59 @@ const Signup = () => {
     formState: { errors },
   } = useForm();
 
-  const onSignup = ({ email, password, username }) => {
-    const Data = {
-      email,
-      password,
-      username,
-    };
+  /* 이메일 중복 검사 확인 */
+  const [isEmailUnique, setIsEmailUnique] = useState(false);
 
-    axios // 수정 필요
-      .post('/signup', Data, {
-        headers: {
-          'Content-Type': 'application/json',
-          'ngrok-skip-browser-warning': true,
-        },
-      })
+  /* 이메일 중복 검사 함수 */
+  const confirmID = ({ email }) => {
+    axios
+      .post('/members/signup/email-check', { email })
       .then((response) => {
-        console.log(response.data);
+        if (response.status === 200) {
+          alert('사용 가능한 이메일입니다.');
+          setIsEmailUnique(true);
+        } else {
+          alert('중복된 이메일입니다.');
+          emailInputRef.current.focus();
+        }
       })
       .catch((error) => {
         console.error(error);
       });
   };
 
+  /* 회원가입 성공 시 라우팅 내비게이터 */
+  const navigate = useNavigate();
+
+  /* 회원가입 폼 제출 함수 */
+  const onSignup = ({ email, password, username }) => {
+    if (isEmailUnique) {
+      const Data = {
+        email,
+        password,
+        username,
+      };
+
+      axios
+        .post('/members/signup', Data)
+        .then((response) => {
+          if (response.status === 200) {
+            alert('회원가입이 완료되었습니다.');
+            navigate('/home');
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    } else {
+      alert('이메일 중복을 확인하세요.');
+    }
+  };
+
+  /* 이메일 중복 시 재입력을 위한 useRef */
+  const emailInputRef = useRef(null);
+
+  /* 비밀번호 일치를 위한 useRef */
   const password = useRef();
   password.current = watch('password');
 
@@ -42,16 +74,21 @@ const Signup = () => {
       <div className={styles.title}>Hongsam IDE</div>
       <form className={styles.form} onSubmit={handleSubmit(onSignup)}>
         <label>ID</label>
-        <input
-          name="email"
-          type="text"
-          autoComplete="on"
-          placeholder="이메일 형식으로 입력해주세요."
-          {...register('email', {
-            required: true,
-            pattern: /^\S+@\S+$/i,
-          })}
-        />
+        <div className={styles.id}>
+          <input
+            name="email"
+            type="text"
+            autoComplete="on"
+            placeholder="이메일 형식으로 입력해주세요."
+            {...register('email', {
+              required: true,
+              pattern: /^\S+@\S+$/i,
+            })}
+          />
+          <button className={styles.confirmID_btn} onClick={confirmID}>
+            중복 확인
+          </button>
+        </div>
         {errors.email && errors.email.type === 'required' && (
           <p>이 칸을 입력해주세요.</p>
         )}
@@ -64,7 +101,7 @@ const Signup = () => {
           name="password"
           type="password"
           autoComplete="on"
-          placeholder="영문+숫자+특수문자 조합의 7~15자 내외로 입력해주세요."
+          placeholder="영문+숫자+특수문자 조합의 7~15자로 입력해주세요."
           {...register('password', {
             required: true,
             pattern:
@@ -99,7 +136,6 @@ const Signup = () => {
         <label>Name</label>
         <input
           name="username"
-          type="text"
           autoComplete="on"
           placeholder="이름을 10자 이내로 입력해주세요."
           {...register('username', {
@@ -114,8 +150,12 @@ const Signup = () => {
           <p>10자 이내로 입력해주세요.</p>
         )}
 
-        <button className={styles.btn} type="submit">
-          Login
+        <button
+          className={styles.submit_btn}
+          disabled={!isEmailUnique}
+          type="submit"
+        >
+          Sign up
         </button>
       </form>
     </div>
