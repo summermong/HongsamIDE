26e4133618ec540acd1473440f1a96af0b3bee8a
강경규 <qwzx16@naver.com>
2023-09-25 23:25:44 +0900

    feat: axiosimport & 기존 코드 가져오는 get 요청 및 서버로 작성 코드를 보내주는 POST요청 임시 작성 완료

--- front-ide/src/components/ide/JavaCodeEditor.js
+++ front-ide/src/components/ide/JavaCodeEditor.js
@@ -1,9 +1,10 @@
 /** @format */
 
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Editor, { useMonaco } from '@monaco-editor/react';
 import SolarizedLigthTheme from 'monaco-themes/themes/Solarized-light.json';
 import { javaDefaultValue } from '../../defaultValues';
+import axios from 'axios';
 
 import ResultTerm from './ResultTerm';
 import IdeTopBar from './IdeTopBar';
@@ -11,6 +12,7 @@ import IdeTopBar from './IdeTopBar';
 export default function JavaCodeEditor() {
   const monaco = useMonaco();
   const editorRef = useRef(null);
+  const [code, setCode] = useState('');
 
   const editorOptions = {
     selectOnLineNumbers: true,
@@ -33,16 +35,49 @@ export default function JavaCodeEditor() {
   useEffect(() => {
     if (!monaco) return;
 
-    console.log('here is the monaco instance:', monaco);
+    console.log('monaco instance:', monaco);
 
     monaco.editor.defineTheme('solarizedLigth', SolarizedLigthTheme);
 
     monaco.editor.setTheme('solarizedLigth');
   }, [monaco]);
-  const showValue = () => {
+  const showValue = async () => {
+    const code = editorRef.current.getValue();
     alert(editorRef.current.getValue());
     console.log(JSON.stringify(editorRef.current.getValue()));
+    console.log(code);
+    const formData = new FormData();
+    formData.append('code', JSON.stringify(code));
+    console.log(formData);
+
+    await axios
+      .post('url', formData)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
+
+  const fetchCode = async () => {
+    await axios
+      .get('url')
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const handleEditorChange = (value, event) => {
+    console.log('here is the current model value:', value);
+    setCode(value);
+    console.log('code : ', code);
+  };
+  useEffect(() => {
+    fetchCode();
+  }, []);
 
   return (
     <>
@@ -55,6 +90,7 @@ export default function JavaCodeEditor() {
           defaultValue={javaDefaultValue}
           onMount={handleEditorDidMount}
           options={editorOptions}
+          onChange={handleEditorChange}
         />
         <ResultTerm />
       </div>
