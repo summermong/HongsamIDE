8f213db39d36cc71a53e9a556713870ab94562ee
woo <ldw28517@naver.com>
2023-10-07 16:13:15 +0900

    feat: 람다 -> WAS 오너 확인 요청

--- back-ide/src/main/java/ide/backide/controller/LambdaApiGatewayHandler.java
+++ back-ide/src/main/java/ide/backide/controller/LambdaApiGatewayHandler.java
@@ -1,6 +1,12 @@
 package ide.backide.controller;
 
 import org.springframework.cloud.function.adapter.aws.FunctionInvoker;
+import org.springframework.web.servlet.HandlerInterceptor;
 
-public class LambdaApiGatewayHandler extends FunctionInvoker {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LambdaApiGatewayHandler extends FunctionInvoker{
+
+
 }
--- back-ide/src/main/java/ide/backide/controller/LambdaRunController.java
+++ back-ide/src/main/java/ide/backide/controller/LambdaRunController.java
@@ -1,41 +1,41 @@
-//package ide.backide.controller;
-//
-//import ide.backide.domain.UserRunRequest;
-//import ide.backide.service.CompilerService;
-//import ide.backide.service.FileIOService;
-//import ide.backide.service.S3Service;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Component;
-//
-//import java.io.File;
-//import java.io.IOException;
-//import java.util.function.Function;
-//
-//@Component
-//@RequiredArgsConstructor
-//public class LambdaRunController implements Function<UserRunRequest, String> {
-//
-//    private final S3Service s3Service;
-//    private final CompilerService compilerService;
-//    private final FileIOService fileIOService;
-//
-//
-//    @Override
-//    public String apply(UserRunRequest userRunRequest) {
-//        File file = null;
-//        if (userRunRequest.getRequestCode().equals("") || userRunRequest.getRequestCode() == null) {
-//            return "코드를 올바르게 입력하세요.";
-//        }
-//        try {
-//            file = fileIOService.saveFile(userRunRequest.getRequestCode(), userRunRequest.getQuestionId());
-//        } catch (IOException e) {
-//            return e.getMessage();
-//        }
-//        s3Service.putS3(userRunRequest.getUuid(), file);
-//        try {
-//            return compilerService.compiler(userRunRequest.getQuestionId());
-//        } catch (Exception e) {
-//            return "알 수 없는 예외 발생";
-//        }
-//    }
-//}
+package ide.backide.controller;
+
+import ide.backide.domain.UserRunRequest;
+import ide.backide.service.CompilerService;
+import ide.backide.service.FileIOService;
+import ide.backide.service.S3Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.function.Function;
+
+@Component
+@RequiredArgsConstructor
+public class LambdaRunController implements Function<UserRunRequest, String> {
+
+    private final S3Service s3Service;
+    private final CompilerService compilerService;
+    private final FileIOService fileIOService;
+
+
+    @Override
+    public String apply(UserRunRequest userRunRequest) {
+        File file = null;
+        if (userRunRequest.getRequestCode().equals("") || userRunRequest.getRequestCode() == null) {
+            return "코드를 올바르게 입력하세요.";
+        }
+        try {
+            file = fileIOService.saveFile(userRunRequest.getRequestCode(), userRunRequest.getQuestionId());
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+        s3Service.putS3(userRunRequest.getUuid(), file);
+        try {
+            return compilerService.compiler(userRunRequest.getQuestionId());
+        } catch (Exception e) {
+            return "알 수 없는 예외 발생";
+        }
+    }
+}
