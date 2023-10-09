5ab57c0ffb149b8685f69146fb6c39067a1c0a21
seoyeoning <minalunpark@naver.com>
2023-10-10 00:17:26 +0900

    cors put 추가

--- back-was/src/main/java/was/backwas/config/CorsConfig.java
+++ back-was/src/main/java/was/backwas/config/CorsConfig.java
@@ -11,7 +11,7 @@ package was.backwas.config;
      public void addCorsMappings(CorsRegistry registry) {
          registry.addMapping("/**") // 모든 요청에 대해 CORS 설정 적용
                  .allowedOrigins("https://main.hong-sam.online", "https://ide.hong-sam.online") // 허용할 도메인 지정
-                 .allowedMethods("GET", "POST", "PATCH", "DELETE", "HEAD", "OPTIONS") // 허용할 HTTP 메서드 지정
+                 .allowedMethods("GET", "POST", "PATCH", "DELETE", "HEAD", "OPTIONS","PUT") // 허용할 HTTP 메서드 지정
                  .allowedHeaders("*") // 허용할 헤더 지정
                  .allowCredentials(true);
      }
