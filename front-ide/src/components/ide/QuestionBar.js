899057c76f0776c675644249e644d997821120ea
강경규 <qwzx16@naver.com>
2023-10-10 20:32:34 +0900

    feat: 불필요한 div 삭제 리펙토링

--- front-ide/src/components/ide/QuestionBar.js
+++ front-ide/src/components/ide/QuestionBar.js
@@ -13,14 +13,13 @@ export default function QuestionBar({ leftWidth, handleMouseDown }) {
   const [questionInput, setQuestionInput] = useState('');
   const [questionOutput, setQuestionOutput] = useState('');
   const { questionIdParam } = useParams();
-  console.log('QuestionBarQID : ', questionIdParam);
+
   const textFileUrl = `https://web-ide.s3.ap-northeast-2.amazonaws.com/admin/${questionIdParam}`; // 텍스트 파일의 URL을 여기에 입력
 
   const fetchQuestion = async (subUrl, setState) => {
     await axios
       .get(`${textFileUrl}/${subUrl}`)
       .then((res) => {
-        console.log(res.data);
         setState(res.data);
       })
       .catch((err) => {
@@ -42,28 +41,27 @@ export default function QuestionBar({ leftWidth, handleMouseDown }) {
       }}
     >
       <div
-        className={`${styles.questionItem} overflow-y-scroll w-full border-r`}
+        className={`${styles.questionItem} questionFont overflow-y-scroll w-full border-r`}
       >
-        <div>
-          <div className={`${styles.questionItem} text-3xl p-5 border-b `}>
-            <ReactMarkDown remarkPlugins={[remarkGfm]}>
-              {questionTitle}
-            </ReactMarkDown>
-          </div>
+        <div className={`${styles.questionItem} text-3xl p-5 border-b `}>
+          <ReactMarkDown remarkPlugins={[remarkGfm]}>
+            {questionTitle}
+          </ReactMarkDown>
         </div>
         <div className={`${styles.questionItem} p-5 border-b`}>
+          <p className='text-lg mb-3'>문제 설명</p>
           <ReactMarkDown remarkPlugins={[remarkGfm]}>
             {questionContent}
           </ReactMarkDown>
         </div>
         <div className={`${styles.questionItem} p-5 border-b`}>
-          <p className='text-lg'>입력</p>
+          <p className='text-lg mb-3'>입력</p>
           <ReactMarkDown remarkPlugins={[remarkGfm]}>
             {questionInput}
           </ReactMarkDown>
         </div>
         <div className={`${styles.questionItem} p-5 border-b`}>
-          <p className='text-lg'>출력</p>
+          <p className='text-lg mb-3'>출력</p>
           <ReactMarkDown remarkPlugins={[remarkGfm]}>
             {questionOutput}
           </ReactMarkDown>
